046247a31da395b5318aa0309f4a22f2e498720e
defifofum <defifofum0@gmail.com>
2023-03-21 17:55:56 -0400

    refactor(test): Fixtures - Provide a single generic fixture function to use for each test - Add ability to pull in composable contract deployments into a single fixture function

--- test/Lock.spec.ts
+++ test/Lock.spec.ts
@@ -4,6 +4,7 @@
  * `time`: Adjust block timestamp
  */
 import { mine, time, loadFixture } from '@nomicfoundation/hardhat-network-helpers'
+import { deployOneYearLockFixture } from './fixtures'
 /**
  * hardhat-chai-matchers reference
  * https://hardhat.org/hardhat-chai-matchers/docs/reference
@@ -21,40 +22,36 @@ import { expect } from 'chai'
 import { ethers } from 'hardhat'
 
 describe('Lock', function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60
-    const ONE_GWEI = 1_000_000_000
-
-    const lockedAmount = ONE_GWEI
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS
-
-    // Contracts are deployed using the first signer/account by default
-    const [owner, otherAccount] = await ethers.getSigners()
-
-    const Lock = await ethers.getContractFactory('Lock')
-    const lock = await Lock.deploy(unlockTime, { value: lockedAmount })
-
-    return { lock, unlockTime, lockedAmount, owner, otherAccount }
+  /**
+   * Configurable fixture to use for each test file.
+   *
+   * As only one fixture can be used per test. This fixture intends to batch multiple contract
+   * deployment functions into a single fixture.
+   *
+   * Fixtures improve test efficiency by reusing the same setup in every test.
+   * loadFixture() runs this setup once, snapshots that state,
+   * and resets the Hardhat Network to that snapshot for every test.
+   */
+  async function fixture() {
+    const lockDeployment = await deployOneYearLockFixture(ethers)
+    return { ...lockDeployment }
   }
 
   describe('Deployment', function () {
     it('Should set the right unlockTime', async function () {
-      const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture)
+      const { lock, unlockTime } = await loadFixture(fixture)
 
       expect(await lock.unlockTime()).to.equal(unlockTime)
     })
 
     it('Should set the right owner', async function () {
-      const { lock, owner } = await loadFixture(deployOneYearLockFixture)
+      const { lock, owner } = await loadFixture(fixture)
 
       expect(await lock.owner()).to.equal(owner.address)
     })
 
     it('Should receive and store the funds to lock', async function () {
-      const { lock, lockedAmount } = await loadFixture(deployOneYearLockFixture)
+      const { lock, lockedAmount } = await loadFixture(fixture)
 
       expect(await ethers.provider.getBalance(lock.address)).to.equal(lockedAmount)
     })
@@ -70,13 +67,13 @@ describe('Lock', function () {
   describe('Withdrawals', function () {
     describe('Validations', function () {
       it('Should revert with the right error if called too soon', async function () {
-        const { lock } = await loadFixture(deployOneYearLockFixture)
+        const { lock } = await loadFixture(fixture)
 
         await expect(lock.withdraw()).to.be.revertedWith("You can't withdraw yet")
       })
 
       it('Should revert with the right error if called from another account', async function () {
-        const { lock, unlockTime, otherAccount } = await loadFixture(deployOneYearLockFixture)
+        const { lock, unlockTime, otherAccount } = await loadFixture(fixture)
 
         // We can increase the time in Hardhat Network
         await time.increaseTo(unlockTime)
@@ -86,7 +83,7 @@ describe('Lock', function () {
       })
 
       it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-        const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture)
+        const { lock, unlockTime } = await loadFixture(fixture)
 
         // Transactions are sent using the first signer by default
         await time.increaseTo(unlockTime)
@@ -97,7 +94,7 @@ describe('Lock', function () {
 
     describe('Events', function () {
       it('Should emit an event on withdrawals', async function () {
-        const { lock, unlockTime, lockedAmount } = await loadFixture(deployOneYearLockFixture)
+        const { lock, unlockTime, lockedAmount } = await loadFixture(fixture)
 
         await time.increaseTo(unlockTime)
 
@@ -107,7 +104,7 @@ describe('Lock', function () {
 
     describe('Transfers', function () {
       it('Should transfer the funds to the owner', async function () {
-        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(deployOneYearLockFixture)
+        const { lock, unlockTime, lockedAmount, owner } = await loadFixture(fixture)
 
         await time.increaseTo(unlockTime)
 
