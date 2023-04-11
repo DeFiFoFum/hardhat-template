77822525161e2fe749d1be40713d37bbf2f7a3d1
defifofum <defifofum0@gmail.com>
2023-04-11 17:35:16 -0400

    chore: Update build structure, security patch

---
+++ prepublish.js
@@ -0,0 +1,10 @@
+var pathArgs = process.argv.slice(2)
+
+console.log('Running prepublish.js')
+
+if (pathArgs.length < 1) {
+
+} else {
+    // NOTE: Can handle args here
+    pathArgs.forEach((argument) => argument);
+}
