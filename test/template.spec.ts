7ed41ac52011ce978a3bb7400a0b794c6ee654a7
defifofum <defifofum0@gmail.com>
2023-04-13 21:51:10 -0400

    test: Provide a template

---
+++ test/template.spec.ts
@@ -0,0 +1,29 @@
+import { ethers } from 'hardhat'
+import { mine, time, loadFixture } from '@nomicfoundation/hardhat-network-helpers'
+import { expect } from 'chai'
+import '@nomicfoundation/hardhat-chai-matchers'
+
+import { deployOneYearLockFixture } from './fixtures'
+
+/**
+ * Configurable fixture to use for each test file.
+ *
+ * As only one fixture can be used per test. This fixture intends to batch multiple contract
+ * deployment functions into a single fixture.
+ *
+ * Fixtures improve test efficiency by reusing the same setup in every test.
+ * loadFixture() runs this setup once, snapshots that state,
+ * and resets the Hardhat Network to that snapshot for every test.
+ */
+async function fixture() {
+  const lockDeployment = await deployOneYearLockFixture(ethers)
+  return { ...lockDeployment }
+}
+
+describe('Test Template', function () {
+  it('Should be able to load fixture', async () => {
+    const loadedFixture = await loadFixture(fixture)
+
+    expect(loadedFixture).to.not.be.undefined
+  })
+})
