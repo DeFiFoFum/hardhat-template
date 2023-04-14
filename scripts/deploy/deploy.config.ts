2976f5e2a03a86e3823acb2ead727bb674d1d514
defifofum <defifofum0@gmail.com>
2023-04-13 21:50:59 -0400

    feat(deploy): Add new deployment scheme with DeployManager class

---
+++ scripts/deploy/deploy.config.ts
@@ -0,0 +1,51 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { Networks } from '../../hardhat.config'
+
+/**
+ * Get the deploy config for a given network
+ * @param network
+ * @returns
+ */
+export const getDeployConfig = (network: DeployableNetworks, signers?: SignerWithAddress[]): DeploymentVariables => {
+  const config = deployableNetworkConfig[network]
+  if (!config) {
+    throw new Error(`No deploy config for network ${network}`)
+  }
+  return config(signers)
+}
+
+/**
+ * Extract networks as deployments are needed
+ *
+ * NOTE: Add networks as needed
+ */
+export type DeployableNetworks = Extract<Networks, 'bsc' | 'bscTestnet'>
+
+/**
+ * Deployment Variables for each network
+ *
+ * NOTE: Update variables as needed
+ */
+interface DeploymentVariables {
+  proxyAdminAddress: string
+  adminAddress: string | SignerWithAddress
+  wNative: string
+}
+
+const deployableNetworkConfig: Record<DeployableNetworks, (signers?: SignerWithAddress[]) => DeploymentVariables> = {
+  bsc: (signers?: SignerWithAddress[]) => {
+    return {
+      proxyAdminAddress: '0x',
+      // NOTE: Example of extracting signers
+      adminAddress: signers?.[0] || '0x',
+      wNative: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    }
+  },
+  bscTestnet: (signers?: SignerWithAddress[]) => {
+    return {
+      proxyAdminAddress: '0x',
+      adminAddress: signers?.[0] || '0x',
+      wNative: '0x',
+    }
+  },
+}
--- scripts/deploy/deployLock.ts
+++ scripts/deploy/deployLock.ts
@@ -1,4 +1,6 @@
-import { ethers } from 'hardhat'
+import { ethers, network } from 'hardhat'
+import { getDeployConfig, DeployableNetworks } from './deploy.config'
+import { DeployManager } from './DeployManager'
 
 /**
  * // NOTE: This is an example of the default hardhat deployment approach.
@@ -6,18 +8,23 @@ import { ethers } from 'hardhat'
  * its own task in ../tasks/ organized by date.
  */
 async function main() {
+  const deployManager = new DeployManager()
+  const currentNetwork = network.name as DeployableNetworks
+  // Optionally pass in accounts to be able to use them in the deployConfig
+  const accounts = await ethers.getSigners()
+  const { wNative, adminAddress } = getDeployConfig(currentNetwork, accounts)
+
   const currentTimestampInSeconds = Math.round(Date.now() / 1000)
   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60
   const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS
 
-  const lockedAmount = ethers.utils.parseEther('1')
+  const lockedAmount = ethers.utils.parseEther('.00001')
 
   const Lock = await ethers.getContractFactory('Lock')
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount })
-
-  await lock.deployed()
-
+  const lock = await deployManager.deployContractFromFactory(Lock, [unlockTime, { value: lockedAmount }])
   console.log('Lock with 1 ETH deployed to:', lock.address)
+
+  await deployManager.verifyContracts()
 }
 
 // We recommend this pattern to be able to use async/await everywhere
