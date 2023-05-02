e075e33395f2cdaf65dae1beee33a12eac2018ef
defifofum <defifofum0@gmail.com>
2023-05-02 17:55:30 -0500

    fix: Polygon Testnet verify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -219,7 +219,7 @@ const config: HardhatUserConfig = {
       bsc: getEnv('BSCSCAN_API_KEY'),
       bscTestnet: getEnv('BSCSCAN_API_KEY'),
       polygon: getEnv('POLYGONSCAN_API_KEY'),
-      polygonTestnet: getEnv('POLYGONSCAN_API_KEY'),
+      polygonMumbai: getEnv('POLYGONSCAN_API_KEY'),
     },
     customChains: [
       {
