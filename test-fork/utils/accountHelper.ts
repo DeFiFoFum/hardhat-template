5898fe8644956bfe03228306dba3cee6acaa0db5
defifofum <defifofum0@gmail.com>
2023-06-15 21:22:28 -0500

    feat: Provide helper function to unlock accounts on fork networks

---
+++ test-fork/utils/accountHelper.ts
@@ -0,0 +1,35 @@
+import { ethers, network } from 'hardhat'
+import { impersonateAccount, setBalance } from '@nomicfoundation/hardhat-network-helpers'
+
+/**
+ * Unlock an account and set its starting balance.
+ *
+ * @param address Address to impersonate
+ * @param etherBalance Starting balance of the account
+ * @returns
+ *
+ * Example usage:
+ *
+ * import { unlockSigner } from "./utils/accountHelper";
+ *
+ * describe("MyContract", () => {
+ *   it("should do something with an unlocked signer", async () => {
+ *     const address = "0x000000000000000000000000000000000000dead"; // Example address to impersonate
+ *     const unlockedSigner = await unlockSigner(address);
+ *
+ *     const myContract = await ethers.getContractFactory("MyContract");
+ *     const contract = await myContract.deploy();
+ *
+ *     // Call a contract function using the unlocked signer
+ *     const result = await contract.connect(unlockedSigner).doSomething();
+ *
+ *     expect(result).to.equal(expectedResult);
+ *   });
+ * });
+ *
+ */
+export async function unlockSigner(address: string, etherBalance = '1') {
+  await impersonateAccount(address)
+  await setBalance(address, ethers.utils.parseEther(etherBalance))
+  return ethers.getSigner(address)
+}
