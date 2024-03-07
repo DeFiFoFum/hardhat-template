ebe5b697edcfe322e221871cbea9e7b6bf56f9ee
defifofum <defifofum0@gmail.com>
2024-03-07 17:28:17 -0600

    feat: Add artifacts for Gnosis Safe creations

--- scripts/deploy/deployCreate2Safe.ts
+++ scripts/deploy/deployCreate2Safe.ts
@@ -1,27 +1,33 @@
 import { ethers, network } from 'hardhat'
-import { writeObjectToTsFile } from '../utils'
+import { writeObjectToTsFile } from '../../lib/files'
 import { logger } from '../../hardhat/utils'
 import { Networks } from '../../hardhat'
 import { convertToExplorerUrlForNetwork } from '../../hardhat.config'
 import { getCREATE2SafeInitializer } from './CREATE2/create2Safe'
 import { CREATE2_DEPLOYER } from './CREATE2'
-import { getErrorMessage } from '../utils/getErrorMessage'
+import { getErrorMessage } from '../../lib/getErrorMessage'
 import GnosisSafeL2_Artifact from '../../artifacts-external/GnosisSafeL2.json'
 import { GnosisSafeL2 } from '../../typechain-types'
+import { forkIfHardhat } from './utils/forkDeployHelper'
+import { DeployableNetworks } from './deploy.config'
+import { askYesOrNoQuestion } from '../../lib/prompts/promptUser'
 
 async function main() {
   // NOTE: Deploy safes with different salts
   await deployCreate2Safe(0, 'SecureAdminSafe')
   // await deployCreate2Safe(1, 'TreasurySafe')
+  // await deployCreate2Safe(2, 'PartnerSafe')
   // await deployCreate2Safe(2, 'GeneralAdminSafe')
   // await deployCreate2Safe(3, 'InsecureAdminSafe')
 }
 
 async function deployCreate2Safe(salt: number, name?: string) {
   logger.logHeader('Deploying Gnosis Safe with CREATE2', '🚀')
+  const currentNetwork = network.name as DeployableNetworks
+  const deployConfigNetwork = await forkIfHardhat(currentNetwork, 'mainnet' as DeployableNetworks)
   const [deployer] = await ethers.getSigners()
   const deployerAddress = await deployer.getAddress()
-  if (deployerAddress != CREATE2_DEPLOYER) {
+  if (currentNetwork != 'hardhat' && deployerAddress != CREATE2_DEPLOYER) {
     throw new Error(`Deployer ${deployerAddress} address must be CREATE2_DEPLOYER: ${CREATE2_DEPLOYER}`)
   }
   const { proxyFactory, gnosisSafeSingleton_l2, initializerParams, initializerData } = await getCREATE2SafeInitializer()
@@ -29,26 +35,9 @@ async function deployCreate2Safe(salt: number, name?: string) {
   // -------------------------------------------------------------------------------------------------------------------
   // Salt setup and verification
   // -------------------------------------------------------------------------------------------------------------------
-  // TODO: Add option to pass in salt with command line argument
-  // For now, change this value manually.
-  const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  })
-
-  const question = () =>
-    new Promise((resolve) => {
-      // @notice Once a salt is used on a network, it cannot be used again with the same deployer account.
-      // @notice Use the same salt on a different network, with the same deployer account, to deploy a Safe with the same address
-      readline.question(`Is the salt ${salt} correct? (Y/N) `, (answer: string) => {
-        readline.close()
-        resolve(answer.trim().toUpperCase() === 'Y')
-      })
-    })
-
-  const isSaltCorrect = await question()
-  if (!isSaltCorrect) {
-    throw new Error('Aborted by user: Salt is incorrect.')
+  if (!(await askYesOrNoQuestion(`Is the salt ${salt} correct? (Y/N)`))) {
+    logger.error('Aborted deployment by user.')
+    throw new Error()
   }
 
   // -------------------------------------------------------------------------------------------------------------------
