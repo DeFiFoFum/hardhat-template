d2a9d0b1afa9b6a6bfad6db3851d6081d0131973
defifofum <defifofum0@gmail.com>
2024-12-16 10:33:43 -0800

    feat: Add ContextManager template

---
+++ contracts/mocks/MockToken.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title MockToken
+ * @notice WARN This contract MUST only be used for testing purposes as it has unrestricted mint capabilities
+ */
+contract MockToken is ERC20, Ownable {
+    uint8 private _decimals = 18;
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_) ERC20(name_, symbol_) {
+        _decimals = decimals_;
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+
+    function decimals() public view override returns (uint8) {
+        return _decimals;
+    }
+}
---
+++ test/context-managers/template_ContextManager.ts
@@ -0,0 +1,58 @@
+import { MockToken, MockToken__factory } from '../../typechain-types'
+import { DeployManager } from '../../scripts/deploy/DeployManager'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+
+type Template_ContextManager_Accounts = {
+  admin: SignerWithAddress
+  deployer: SignerWithAddress
+}
+
+type Template_ContextManager_Props = {
+  accounts: Template_ContextManager_Accounts
+  tokens: {
+    principalToken: MockToken
+    payoutToken: MockToken
+  }
+}
+
+export class Template_ContextManager {
+  private _props: Template_ContextManager_Props
+
+  constructor(props: Template_ContextManager_Props) {
+    this._props = props
+  }
+
+  public get props(): Template_ContextManager_Props {
+    return this._props
+  }
+
+  static async createWithBondConfig(
+    accounts: Template_ContextManager_Accounts,
+    principalSupply: string,
+    payoutSupply: string
+  ): Promise<Template_ContextManager> {
+    const deployManager = await DeployManager.create({ signer: accounts.deployer })
+
+    // Deploy tokens
+    const principalToken = await deployManager.deployContract<MockToken__factory>('MockToken', [
+      'PrincipalToken',
+      'BUY',
+      principalSupply,
+    ])
+    const payoutToken = await deployManager.deployContract<MockToken__factory>('MockToken', [
+      'PayoutToken',
+      'SELL',
+      payoutSupply,
+    ])
+
+    const contextManagerProps: Template_ContextManager_Props = {
+      accounts,
+      tokens: {
+        principalToken,
+        payoutToken,
+      },
+    }
+
+    return new Template_ContextManager(contextManagerProps)
+  }
+}
